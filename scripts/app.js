9c88634139409d38b2cea179aca946edc3f2463f
s-arai <macbookpro@9105-109204.local>
2013-08-20 12:34:03 +0900

    create skel

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,114 @@
-;(function (window, document, undefined) {
+;(function (window, document, $, Backbone, _, undefined) {
 
   // some code ..
   console.log("test");
 
-})(window, document);
+
+  var Model = Backbone.Model.extend({
+    initialize: function(){
+    
+    },
+  });
+
+  var Collection = Backbone.Collection.extend({
+    initialize: function(){
+    
+    },
+
+    getByWord: function(word){
+      
+    },
+
+    getByCategory: function(categoryName){
+
+    },
+
+    getByDevice: function(device) {
+
+    },
+
+    getRecent: function(limit) {
+
+    },
+
+    getDeviceList: function(){
+
+    },
+
+    getCategoryList: function(){
+
+    }
+  });
+
+  var IndexPageView = Backbone.View.extend({
+    events: {
+    
+    },
+
+    initialize: function(){
+    
+    },
+
+  });
+
+  var ListPageView = Backbone.View.extend({
+    events: {
+    
+    },
+
+    initialize: function(){
+    
+    }
+  
+  });
+
+  var SearchView = Backbone.View.extend({
+    events: {
+    
+    },
+
+    initialize: function(){
+    
+    }
+  
+  });
+
+  var CategoryListView = Backbone.View.extend({
+    events: {
+    
+    },
+
+    initialize: function(){
+    
+    }
+  
+  });
+
+  var ResultView = Backbone.View.extend({
+    events: {
+    
+    },
+
+    initialize: function(){
+    
+    }
+  
+  });
+
+  var Router = Backbone.Router.extend({
+    routes: {
+      "/": "index",
+      "/list": "list",
+      "/list/device_:device": "device",
+    },
+
+    index: function(){},
+
+    list: function(){},
+
+    device: function(device){}
+  
+  });
+
+
+})(window, document, jQuery, Backbone, _);
