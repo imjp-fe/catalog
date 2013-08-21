3a0ee0564795474e5a3c6921d189981ac97faf26
tomoaki-o <macbookpro@9105-109178.local>
2013-08-21 21:23:48 +0900

    マージした

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,6 @@
 ;(function (window, document, $, Backbone, _, undefined) {
 
-  // some code ..
-  console.log("test");
-
-
+  
   var Model = Backbone.Model.extend({
     initialize: function(){
 
@@ -11,129 +8,158 @@
   });
 
   var Collection = Backbone.Collection.extend({
-    initialize: function(){
 
-    },
+    model: Model,
+
+    url: '/data.json',
 
     getByWord: function(word){
+      var objects = [],
+          words = [],
+          scope = ['summary', 'title', 'creator', 'tags', 'device', 'browser'],
+          noIncludeWords = ['summary'],
+          caches = [],
+          word = word.toLowerCase().replace(/^(?:\s*)?(\S+?)(?:\s*)?$/, '$1'),
+          reg = new RegExp('\\s*?' + word + '\\s*?');
 
-      /* DUMMY jsonデータ */
-      return [
-        {
-          "URL":"http://hogehoge.com",
-          "title":"wordによる絞り込み１",
-          "summary":"さっぱり生麺タイプ サマリーが入ります。サマリーが入ります。",
-          "thumb":"/images/img_thumb.gif"
-        },
-        {
-          "URL":"http://hogehoge.com",
-          "title":"wordによる絞り込み２",
-          "summary":"こってり生麺タイプ サマリーが入ります。サマリーが入ります。サマリーが入ります。サマリーが入ります。",
-          "thumb":"/images/img_thumb.gif"
+      var isDuplicate = function(str){
+        for(var i = 0, l = words.length; i < l; i++){
+          if(str.toLowerCase() == words[i].toLowerCase()){
+            return true;
+          }
         }
-      ];
+        return false;
+      };
+
+      var isIncludeWords = function(str) {
+        for(var i = 0, l = noIncludeWords.length; i < l; i++){
+          if(str.toLowerCase() == noIncludeWords[i].toLowerCase()){
+            return false;
+          }
+        }
+        return true;
+      }
+
+      var add = function(key, word, model) {
+        if (reg.test(word.toLowerCase())) {          
+          if (!isDuplicate(word) && isIncludeWords(key)) words.push(word);
+          objects.push(model.toJSON());
+        }
+      }
+
+      for (var i = 0, l = this.models.length; i < l; i++) {
+        var pick = this.models[i].pick(scope);
+        for (key in pick) {
+          if (_.isArray(pick[key])) {
+            for (var j = 0, k = pick[key].length; j < k; j++) {
+              add(key, pick[key][j], this.models[i]);
+            }
+            continue;
+          }
+          if (_.isString(pick[key])) {
+            add(key, pick[key], this.models[i]);
+            continue;
+          }
+        }
+      };
+      
+      return { objects: objects, words: words };
     },
 
     getByCategory: function(categoryName){
-      /* DUMMY jsonデータ */
-      return [
-        {
-          "URL":"http://hogehoge.com",
-          "title":"カテゴリーによる絞り込み１",
-          "summary":"さっぱり生麺タイプ サマリーが入ります。サマリーが入ります。",
-          "thumb":"/images/img_thumb.gif"
-        },
-        {
-          "URL":"http://hogehoge.com",
-          "title":"カテゴリーによる絞り込み２",
-          "summary":"こってり生麺タイプ サマリーが入ります。サマリーが入ります。サマリーが入ります。サマリーが入ります。",
-          "thumb":"/images/img_thumb.gif"
-        }
-      ];
+
+      var res = '';
+
+      console.log(this.models);
+
+      return res;
+
+      // var res = '';
+
+      // var list = this.where({"man-hour": 160}),
+      //     _list = [];
+
+      // list.forEach(function (el, i) {
+      //   _list[i] = el.toJSON();
+      // });
+      
+      // console.log(_.pluck(_list, 'man-hour'));
+      
+      // return res;
+
     },
 
     getByDevice: function(device) {
-      /* DUMMY jsonデータ */
-      return [
-        {
-          "URL":"http://hogehoge.com",
-          "title":"デバイスによる絞り込み１",
-          "summary":"さっぱり生麺タイプ サマリーが入ります。サマリーが入ります。",
-          "thumb":"/images/img_thumb.gif"
-        },
-        {
-          "URL":"http://hogehoge.com",
-          "title":"デバイスによる絞り込み２",
-          "summary":"こってり生麺タイプ サマリーが入ります。サマリーが入ります。サマリーが入ります。サマリーが入ります。",
-          "thumb":"/images/img_thumb.gif"
+      var objects = [];
+      _.each(this.models, function (el, i) {
+        var object = el.toJSON(),
+            prop = object.device;
+        if (_.isArray(prop)) {              // if prop is "Array"
+          _.each(prop, function (val, j) {
+            if (val === device) {
+              objects.push(object);
+            }
+          });
+        } else {                            // if prop is "string"
+          if (prop === device) {
+            objects.push(object);
+          }
         }
-      ];
+      });
+      return objects;
     },
 
     getRecent: function(limit) {
-      /* DUMMY jsonデータ */
-      return [
-        {
-          "URL":"http://hogehoge.com",
-          "title":"塩ラーメン",
-          "summary":"さっぱり生麺タイプ サマリーが入ります。サマリーが入ります。",
-          "thumb":"/images/img_thumb.gif",
-          "creator":"Hoge",
-          "email":"hoge@hoge.com",
-          "version":"0.1",
-          "create date":"2002-09-24+09:00",
-          "update date":"2002-10-24+09:00",
-          "license":"",
-          "tags":["hoge","poop"],
-          "device":["PC", "iPhone"],
-          "Browser":["Chrome", "Firefox"],
-          "man-hour":34
-        },
-        {
-          "URL":"http://hogehoge.com",
-          "title":"みそラーメン",
-          "summary":"こってり生麺タイプ サマリーが入ります。サマリーが入ります。サマリーが入ります。サマリーが入ります。",
-          "thumb":"/images/img_thumb.gif",
-          "creator":"Hoge",
-          "email":"hoge@hoge.com",
-          "version":"0.1",
-          "create date":"2002-09-24+09:00",
-          "update date":"2002-10-24+09:00",
-          "license":"",
-          "tags":["hoge","poop"],
-          "device":["PC", "iPhone"],
-          "Browser":["Chrome", "Firefox"],
-          "man-hour":34
-        },
-        {
-          "URL":"http://hogehoge.com",
-          "title":"豚骨ラーメン",
-          "summary":"こってり生麺タイプ サマリーが入ります。サマリーが入ります。サマリーが入ります。サマリーが入ります。",
-          "thumb":"/images/img_thumb.gif",
-          "creator":"Hoge",
-          "email":"hoge@hoge.com",
-          "version":"0.1",
-          "create date":"2002-09-24+09:00",
-          "update date":"2002-10-24+09:00",
-          "license":"",
-          "tags":["hoge","poop"],
-          "device":["PC", "iPhone"],
-          "Browser":["Chrome", "Firefox"],
-          "man-hour":34
-        }
-      ];
+      var objects = [],
+          sotred = _(this.models).sortBy(function (object) {
+            return Date.parse(object.toJSON().create_date);
+          });
+      sotred = sotred.slice(0, limit);
+      _.each(sotred, function (el, i) {
+        objects[i] = el.toJSON();
+      });
+      return objects;
     },
 
     getDeviceList: function(){
-      /* DUMMY jsonデータ */
-      return ['PC', 'Smartphone', 'tablet'];
+      var objects = [];
+      _.each(this.models, function (el, i) {
+        objects[i] = el.toJSON().device;
+      });
+      objects = _.uniq(objects);
+      return objects;
     },
 
     getCategoryList: function(){
-      /* DUMMY jsonデータ */
-      return ['ajax', 'animation', 'API', 'js'];
+      var objects = [];
+      _.each(this.models, function (el, i) {
+        objects[i] = el.toJSON().tags;
+      });
+      objects = _.union(objects[0]);
+      return objects;
     }
+
   });
+
+  var collection = new Collection();
+
+
+  collection.fetch().done(function (res) {
+    //console.log(res);
+    //console.log(collection.models);
+    
+    
+    // console.log(collection.getByWord('アプリ'));
+    // console.log(collection.getByCategory());
+
+
+    console.log(collection.getByDevice('sp'));
+    console.log(collection.getRecent(3));
+    console.log(collection.getDeviceList());
+    console.log(collection.getCategoryList());
+    
+  });
+
 
   var IndexPageView = Backbone.View.extend({
     events: {
@@ -247,9 +273,9 @@
   });
 
 
-  var collection = new Collection();
-  var categoryListView = new CategoryListView();
-  var resultView = new ResultView();
+  // var collection = new Collection();
+  // var categoryListView = new CategoryListView();
+  // var resultView = new ResultView();
 
 
 })(window, document, jQuery, Backbone, _);
