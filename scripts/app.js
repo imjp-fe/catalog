e3c79cfa308440a4449bf83cc0d8d8f1a4f3e023
Yoko Hayashi <yokoh9@gmail.com>
2013-08-21 16:14:03 +0900

    add to app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,178 +1,235 @@
 ;(function (window, document, $, Backbone, _, undefined) {
 
+  // some code ..
+  console.log("test");
+
+
   var Model = Backbone.Model.extend({
-
     initialize: function(){
-    }
 
+    },
   });
 
-
   var Collection = Backbone.Collection.extend({
-
-    model: Model,
-
-    url: '/data.json',
-
     initialize: function(){
-      //console.log(this.models);
+
     },
 
     getByWord: function(word){
 
-      var res = '';
-
-      var list = this.where({"man-hour": 160}),
-          _list = [];
-
-      list.forEach(function (el, i) {
-        _list[i] = el.toJSON();
-      });
-      
-      console.log(_.pluck(_list, 'man-hour'));
-      
-      return res;
-      
+      /* DUMMY jsonデータ */
+      return [
+        {
+          "URL":"http://hogehoge.com",
+          "title":"wordによる絞り込み１",
+          "summary":"さっぱり生麺タイプ サマリーが入ります。サマリーが入ります。",
+          "thumb":"/images/img_thumb.gif"
+        },
+        {
+          "URL":"http://hogehoge.com",
+          "title":"wordによる絞り込み２",
+          "summary":"こってり生麺タイプ サマリーが入ります。サマリーが入ります。サマリーが入ります。サマリーが入ります。",
+          "thumb":"/images/img_thumb.gif"
+        }
+      ];
     },
 
     getByCategory: function(categoryName){
-
-      var res = '';
-
-      console.log(this.models);
-
-      return res;
-
+      /* DUMMY jsonデータ */
+      return [
+        {
+          "URL":"http://hogehoge.com",
+          "title":"カテゴリーによる絞り込み１",
+          "summary":"さっぱり生麺タイプ サマリーが入ります。サマリーが入ります。",
+          "thumb":"/images/img_thumb.gif"
+        },
+        {
+          "URL":"http://hogehoge.com",
+          "title":"カテゴリーによる絞り込み２",
+          "summary":"こってり生麺タイプ サマリーが入ります。サマリーが入ります。サマリーが入ります。サマリーが入ります。",
+          "thumb":"/images/img_thumb.gif"
+        }
+      ];
     },
 
     getByDevice: function(device) {
-      var objects = [];
-      _.each(this.models, function (el, i) {
-        var object = el.toJSON(),
-            prop = object.device;
-        if (_.isArray(prop)) {              // if prop is "Array"
-          _.each(prop, function (val, j) {
-            if (val === device) {
-              objects.push(object);
-            }
-          });
-        } else {                            // if prop is "string"
-          if (prop === device) {
-            objects.push(object);
-          }
+      /* DUMMY jsonデータ */
+      return [
+        {
+          "URL":"http://hogehoge.com",
+          "title":"デバイスによる絞り込み１",
+          "summary":"さっぱり生麺タイプ サマリーが入ります。サマリーが入ります。",
+          "thumb":"/images/img_thumb.gif"
+        },
+        {
+          "URL":"http://hogehoge.com",
+          "title":"デバイスによる絞り込み２",
+          "summary":"こってり生麺タイプ サマリーが入ります。サマリーが入ります。サマリーが入ります。サマリーが入ります。",
+          "thumb":"/images/img_thumb.gif"
         }
-      });
-      return objects;
+      ];
     },
 
     getRecent: function(limit) {
-      var objects = [];
-      for (var i = 0; i < limit; i++) {
-        objects[i] = this.models[i].toJSON();
-      };
-      return objects;
+      /* DUMMY jsonデータ */
+      return [
+        {
+          "URL":"http://hogehoge.com",
+          "title":"塩ラーメン",
+          "summary":"さっぱり生麺タイプ サマリーが入ります。サマリーが入ります。",
+          "thumb":"/images/img_thumb.gif",
+          "creator":"Hoge",
+          "email":"hoge@hoge.com",
+          "version":"0.1",
+          "create date":"2002-09-24+09:00",
+          "update date":"2002-10-24+09:00",
+          "license":"",
+          "tags":["hoge","poop"],
+          "device":["PC", "iPhone"],
+          "Browser":["Chrome", "Firefox"],
+          "man-hour":34
+        },
+        {
+          "URL":"http://hogehoge.com",
+          "title":"みそラーメン",
+          "summary":"こってり生麺タイプ サマリーが入ります。サマリーが入ります。サマリーが入ります。サマリーが入ります。",
+          "thumb":"/images/img_thumb.gif",
+          "creator":"Hoge",
+          "email":"hoge@hoge.com",
+          "version":"0.1",
+          "create date":"2002-09-24+09:00",
+          "update date":"2002-10-24+09:00",
+          "license":"",
+          "tags":["hoge","poop"],
+          "device":["PC", "iPhone"],
+          "Browser":["Chrome", "Firefox"],
+          "man-hour":34
+        },
+        {
+          "URL":"http://hogehoge.com",
+          "title":"豚骨ラーメン",
+          "summary":"こってり生麺タイプ サマリーが入ります。サマリーが入ります。サマリーが入ります。サマリーが入ります。",
+          "thumb":"/images/img_thumb.gif",
+          "creator":"Hoge",
+          "email":"hoge@hoge.com",
+          "version":"0.1",
+          "create date":"2002-09-24+09:00",
+          "update date":"2002-10-24+09:00",
+          "license":"",
+          "tags":["hoge","poop"],
+          "device":["PC", "iPhone"],
+          "Browser":["Chrome", "Firefox"],
+          "man-hour":34
+        }
+      ];
     },
 
     getDeviceList: function(){
-
-      var res = '';
-
-      console.log(this.models);
-
-      return res;
-
+      /* DUMMY jsonデータ */
+      return ['PC', 'Smartphone', 'tablet'];
     },
 
     getCategoryList: function(){
-
-      var res = '';
-
-      console.log(this.models);
-
-      return res;
-
+      /* DUMMY jsonデータ */
+      return ['ajax', 'animation', 'API', 'js'];
     }
-
   });
 
-  var collection = new Collection();
-
-
-  collection.fetch().done(function (res) {
-    //console.log(res);
-    //console.log(collection.models);
-    // console.log(collection.getByWord());
-    // console.log(collection.getByCategory());
-
-
-    console.log(collection.getByDevice('sp'));
-    // console.log(collection.getRecent(4));
-    
-  });
-
-
-
-
-
-
-
-  /*!
-   * VIEW
-   */
   var IndexPageView = Backbone.View.extend({
     events: {
-    
+
     },
 
     initialize: function(){
-    
+
     },
 
   });
 
   var ListPageView = Backbone.View.extend({
     events: {
-    
+
     },
 
     initialize: function(){
-    
+
     }
-  
+
   });
 
   var SearchView = Backbone.View.extend({
     events: {
-    
+
     },
 
     initialize: function(){
-    
+
     }
-  
+
   });
 
   var CategoryListView = Backbone.View.extend({
-
     events: {
-      
-    },
 
+    },
     initialize: function(){
-    
+      var devices = collection.getDeviceList();
+      var tags = collection.getCategoryList();
+      this.renderDevice(devices);
+      this.renderTags(tags);
+    },
+    renderDevice: function(devices){
+      $.each(devices, function(i){
+        var $deviceList = $('.device ul');
+        $deviceList.append('<li><a href="/device/' + devices[i] + '">' + devices[i] + '</a></li>');
+      });
+    },
+    renderTags: function(tags){
+      $.each(tags, function(i){
+        var $tagList = $('.tags ul');
+        $tagList.append('<li><a href="/tag/' + tags[i] + '">' + tags[i] + '</a></li>');
+      });
     }
-  
   });
+
 
   var ResultView = Backbone.View.extend({
     events: {
-    
-    },
 
+    },
     initialize: function(){
-    
+      this.renderRecent();
+      this.renderWord();
+      this.renderCategory();
+      this.renderDevice();
+    },
+    renderRecent: function(){
+      var itemRecent = collection.getRecent();
+      var ul = $('#recentList ul');
+      this.render(itemRecent, ul);
+    },
+    renderWord: function(){
+      var itemWord = collection.getByWord();
+      var ul = $('#wordList ul');
+      this.render(itemWord, ul);
+    },
+    renderCategory: function(){
+      var itemCategory = collection.getByCategory();
+      var ul = $('#categoryList ul');
+      this.render(itemCategory, ul);
+    },
+    renderDevice: function(){
+      var itemDevice = collection.getByDevice();
+      var ul = $('#deviceList ul');
+      this.render(itemDevice, ul);
+    },
+    render: function(items, ul){
+      var tpl = '<% _.each(obj, function(i){ %>\
+        <li><a href="<%= i.URL %>"><img src="<%= i.thumb %>" alt=""><span class="title"><%= i.title %></span><span class="summary"><%= i.summary %></span></a></li>\
+        <% }); %>';
+      var list = _.template(tpl, {obj:items}); // 配列items ⇒ オブジェクトの形にして _.templateに渡す
+      ul.append(list);
     }
-  
   });
 
   var Router = Backbone.Router.extend({
@@ -187,8 +244,13 @@
     list: function(){},
 
     device: function(device){}
-  
+
   });
+
+
+  var collection = new Collection();
+  var categoryListView = new CategoryListView();
+  var resultView = new ResultView();
 
 
 })(window, document, jQuery, Backbone, _);
